c712cd2c9811ccbc8b8b0d0b927aa823bd895fd4
tete18 <57841586+tete18@users.noreply.github.com>
2019-11-16 21:04:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,8 @@ client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = ("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+  message.channel.send("**للأسف لا تمتلك صلاحية `ADMINISTRATOR`**");
+                
   var argresult = args.join(' '); 
   message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
  m.send(`${argresult}\n ${m}`);
